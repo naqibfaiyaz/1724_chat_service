d1e43243b43f929f5687759056cbcf3dd6956bca
naqibfaiyaz <naqib3110@gmail.com>
2023-11-26 12:33:34 -0500

    pushing frontend - client; api gateway - krakend; auth_MS - auth0; chat_MS - backend

---
+++ backend/chat/config.py
@@ -0,0 +1,36 @@
+import os
+
+import redis
+from werkzeug.utils import import_string
+from dotenv import load_dotenv
+load_dotenv()
+
+auth0_domain = os.getenv('AUTH0_DOMAIN')
+
+
+
+class Config(object):
+    # Parse redis environment variables.
+    redis_endpoint_url = os.getenv("REDIS_ENDPOINT_URL", "127.0.0.1:6379")
+    REDIS_HOST, REDIS_PORT = tuple(redis_endpoint_url.split(":"))
+    REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+    SECRET_KEY = os.getenv("SECRET_KEY", "Optional default value")
+    SESSION_TYPE = "redis"
+    redis_client = redis.Redis(
+        host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD
+    )
+    SESSION_REDIS = redis_client
+    # TODO: Auth...
+
+
+class ConfigDev(Config):
+    # DEBUG = True
+    pass
+
+
+class ConfigProd(Config):
+    pass
+
+
+def get_config() -> Config:
+    return import_string(os.getenv("CHAT_CONFIG", "chat.config.ConfigDev"))
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+// @ts-check
+import React from "react";
+
+const Navbar = () => {
+  /**
+   * @type {[{
+   *  heroku?: string;
+   *  google_cloud?: string;
+   *  vercel?: string;
+   *  github?: string;
+   *  }, React.Dispatch<any>]}
+   */
+  
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <span className="navbar-brand">Chat demo</span>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
