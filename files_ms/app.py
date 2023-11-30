36014ad20c2be3e26c9a8dbd4c629dbde3e969e9
Suyash027 <sagarwal0027@gmail.com>
2023-11-30 11:11:08 -0500

    deployed as ms

---
+++ files_ms/app.py
@@ -0,0 +1,58 @@
+import os
+import requests
+import mimetypes
+from flask import Flask, request, jsonify
+from urllib.parse import quote  # Import the quote function from urllib.parse
+app = Flask(__name__)
+
+api_url = 'https://g3b0zrn210.execute-api.us-east-1.amazonaws.com/dev/ece1724/'
+
+def file_put(file_name, bucket_file):
+    print(file_name, bucket_file)
+
+    url=api_url+bucket_file
+    path = os.path.join('upload', file_name)
+    payload = open(path,'rb')
+    mime_type, _ = mimetypes.guess_type(file_name)
+    content = mime_type if mime_type else 'application/octet-stream'
+    response = requests.request("PUT", url, headers={'Content-Type': content}, data=payload)
+    # print(response.status_code)
+
+def file_get(file_name, bucket_file):
+    print(file_name, bucket_file)
+    url=api_url+bucket_file
+    response = requests.get(url)
+    path = os.path.join('download', file_name)
+
+    #saving the file
+    with open(path, "wb") as file:
+        file.write(response.content)
+    # print(response.status_code)
+
+@app.route('/upload', methods=['POST','PUT'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    file_name = file.filename
+    bucket_file =  file_name
+    file.save(os.path.join('upload', file_name))
+
+    file_put(file_name=file_name, bucket_file=bucket_file)
+
+    return jsonify({'message': 'File uploaded successfully'}), 200
+
+@app.route('/download/<file_name>', methods=['GET'])
+def download_file(file_name):
+    bucket_file = file_name
+    file_get(file_name=file_name, bucket_file=bucket_file)
+
+    return jsonify({'message': 'File downloaded successfully'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5010)
