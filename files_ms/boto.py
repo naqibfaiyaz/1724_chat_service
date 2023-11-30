fec669fce3da7dc327768750d7562efeda8e0760
Suyash027 <sagarwal0027@gmail.com>
2023-11-30 10:00:02 -0500

    api put/get

--- files_ms/boto.py
+++ files_ms/boto.py
@@ -1,11 +1,10 @@
-import os
+#To use boto3
 import boto3
-import requests
 from boto3.session import Session
 # Set the below variables to use Boto3
 aws_access_key_ID = 'xxxxx'
 aws_secret_key = 'xxxxx'
-api_url = 'https://g3b0zrn210.execute-api.us-east-1.amazonaws.com/dev/ece1724/'
+api_url = 'xxxxxx'
 
 def s3_upload(file_name, bucket_name='ece1724',bucket_file='botosample6.png'):   
     s3_client = boto3.client('s3',aws_access_key_id=aws_access_key_ID, aws_secret_access_key=aws_secret_key )     
@@ -26,36 +25,6 @@ def s3_delete(bucket_file):
 
     my_bucket.delete_objects(Delete={'Objects': [{'Key': bucket_file}]})
 
-def file_put(file_name, bucket_file):
-    url=api_url+bucket_file
-    path = os.path.join('upload', file_name)
-    # payload = open(f'upload/{file_name}','rb')
-    payload = open(path,'rb')
-
-    if file_name.split('.')[-1] == 'png':
-        content='image/png'
-    if file_name.split('.')[-1]== 'jpeg':
-        content='image/jpeg'
-    if file_name.split('.')[-1]== 'txt':
-        content='text/plain'
-    if file_name.split('.')[-1]== 'pdf':
-        content='application/pdf'    
-
-    response = requests.request("PUT", url, headers={'Content-Type': content}, data=payload)
-    print(response.status_code)
-
-def file_get(file_name, bucket_file):
-    url=api_url+bucket_file
-    response = requests.get(url)
-    path = os.path.join('download', file_name)
-    #saving the file
-    with open(path, "wb") as file:
-        file.write(response.content)
-
-    print(response.status_code)
-
 # s3_upload(file_name=file_name, bucket_file='test1.png')
-# # s3_download(file_name='test1_download.png', bucket_file='test1.png')
+# s3_download(file_name='test1_download.png', bucket_file='test1.png')
 # s3_delete('test1.png')
-file_put(file_name='text1.txt', bucket_file='upload2.txt')
-file_get(file_name='download2.txt', bucket_file='upload2.txt')
