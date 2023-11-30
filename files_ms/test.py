7f69a549bf7354799d0396b61925158f2fc4f27e
Suyash027 <sagarwal0027@gmail.com>
2023-11-30 10:01:53 -0500

    api changes

--- files_ms/test.py
+++ files_ms/test.py
@@ -1,30 +1,7 @@
 import os
-import boto3
 import requests
-from boto3.session import Session
-# Set the below variables to use Boto3
-aws_access_key_ID = 'xxxxx'
-aws_secret_key = 'xxxxx'
+
 api_url = 'https://g3b0zrn210.execute-api.us-east-1.amazonaws.com/dev/ece1724/'
-
-def s3_upload(file_name, bucket_name='ece1724',bucket_file='botosample6.png'):   
-    s3_client = boto3.client('s3',aws_access_key_id=aws_access_key_ID, aws_secret_access_key=aws_secret_key )     
-    response=s3_client.upload_file(file_name,bucket_name,bucket_file)
-    print(response)
-    # print(file_name,'uploaded on bucket',bucket_name,'successfully')
-
-def s3_download(file_name, bucket_name='ece1724',bucket_file='botosample4.png'):  
-    s3_client = boto3.client('s3',aws_access_key_id=aws_access_key_ID, aws_secret_access_key=aws_secret_key )      
-    s3_client.download_file(bucket_name,bucket_file,file_name)
-
-
-def s3_delete(bucket_file):
-    # s3_client = boto3.client('s3',aws_access_key_id=aws_access_key_ID, aws_secret_access_key=aws_secret_key )
-    session = Session(aws_access_key_id=aws_access_key_ID,aws_secret_access_key=aws_secret_key)
-    s3_resource = session.resource('s3')
-    my_bucket = s3_resource.Bucket("ece1724")
-
-    my_bucket.delete_objects(Delete={'Objects': [{'Key': bucket_file}]})
 
 def file_put(file_name, bucket_file):
     url=api_url+bucket_file
@@ -54,8 +31,6 @@ def file_get(file_name, bucket_file):
 
     print(response.status_code)
 
-# s3_upload(file_name=file_name, bucket_file='test1.png')
-# # s3_download(file_name='test1_download.png', bucket_file='test1.png')
-# s3_delete('test1.png')
+
 file_put(file_name='text1.txt', bucket_file='upload2.txt')
 file_get(file_name='download2.txt', bucket_file='upload2.txt')
