1a7275ae98a58dab58c76b14f6ee81ec9bdab61d
Youssef Shoala <youssefshoala@hotmail.com>
2023-12-07 00:21:45 +0000

    editing start.sh

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -3,10 +3,11 @@
  chmod 700 get_helm.sh
  ./get_helm.sh
  helm repo add bitnami https://charts.bitnami.com/bitnami
+ helm repo add prometheus-community https://prometheus-community.github.io/helm-chart
  helm repo update
  helm install redis bitnami/redis
  helm install metrics-server bitnami/metrics-server
- helm install grafana bitnami/grafana
+ helm install stable prometheus-community/kube-prometheus-stack
  kubectl create -f auth-ms-deployment-definition.yaml
  kubectl create -f auth-ms-autoscale.yaml
  kubectl create -f auth-ms-service.yaml
